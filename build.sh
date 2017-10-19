522ca5d8a7ea385577db3e01aba1e5b5b316ec77
Cristian Maglie <c.maglie@arduino.cc>
2017-10-19 17:07:33 +0200

    Released 0.9.2

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@
 # invalidate any other reasons why the executable file might be covered by
 # the GNU General Public License.
 
-REV=0.9.1
+REV=0.9.2
 ZIP_FILENAME=WiFi101-Updater-ArduinoIDE-Plugin-$REV
 REQUIRED_JARS="pde.jar arduino-core.jar jssc-2.8.0-arduino1.jar bcpg-jdk15on-152.jar bcprov-jdk15on-152.jar"
 
